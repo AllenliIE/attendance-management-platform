c5bc361a001ae81af4d6976eb0a75cd2041d9c55
AllenliIE <allen.li.service@gmail.com>
2023-01-08 00:55:50 +0800

    feat: modify frontend structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="attendance-management-platform">
+    <router-view />
+  </div>
 </template>
 
 <script>
@@ -13,26 +11,3 @@
     name: 'App'
   }
 </script>
-
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  nav {
-    padding: 30px;
-  }
-
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
-</style>
