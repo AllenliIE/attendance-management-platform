427f8a54db0bcb2b08147eae3d2a85150efc304c
AllenliIE <allen.li.service@gmail.com>
2023-01-07 17:26:53 +0800

    create backend file

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.info(`Attendance-Management-Platform is listening on port:${port}!`);
+});
+
+module.exports = app;
