e4d30b20e810b28a23e61bd75a08c3caa1a71740
AllenliIE <allen.li.service@gmail.com>
2023-01-08 22:41:46 +0800

    feat: use uuid to modify navtabs

--- frontend/src/components/NavTabs.vue
+++ frontend/src/components/NavTabs.vue
@@ -1,21 +1,38 @@
 <template>
   <aside class="col-2 text-center" style="background-color: #14597f">
     <ul class="nav nav-tabs">
-      <li class="nav-item col m-4">
-        <router-link to="/attendance/profile" class="nav-link row text-white">
-          <p class="fs-4">Profile</p>
-        </router-link>
-      </li>
-      <li class="nav-item col m-4">
-        <router-link to="/attendance/reader" class="nav-link row text-white">
-          <p class="fs-4">Reader</p>
-        </router-link>
-      </li>
-      <li class="nav-item col m-4">
-        <router-link to="/attendance/checkin" class="nav-link row text-white">
-          <p class="fs-4">Check</p>
+      <li v-for="tab in tabs" :key="tab.id" class="nav-item col m-4">
+        <router-link :to="tab.path" class="nav-link row text-white">
+          <p class="fs-4">{{ tab.title }}</p>
         </router-link>
       </li>
     </ul>
   </aside>
 </template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+export default {
+  data() {
+    return {
+      tabs: [
+        {
+          id: uuidv4(),
+          title: "Profile",
+          path: "/attendance/profile",
+        },
+        {
+          id: uuidv4(),
+          title: "Reader",
+          path: "/attendance/reader",
+        },
+        {
+          id: uuidv4(),
+          title: "CheckIn",
+          path: "/attendance/checkin",
+        },
+      ],
+    };
+  },
+};
+</script>
