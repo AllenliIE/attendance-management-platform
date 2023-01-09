7d3d0afedc16f06a0b44f24dd6c94f7e629a2d9d
AllenliIE <allen.li.service@gmail.com>
2023-01-09 08:11:23 +0800

    feat: add admin router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const attendController = require("../controllers/attendance-controller");
+const admin = require("./modules/admin");
 
+router.use("/admin", admin);
 router.get("/attendance", attendController.getAttendance);
 router.use("/", (req, res) => res.redirect("/attendance"));
 
